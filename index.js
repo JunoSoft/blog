1f70735089e68c394ee71b35a59e16c91ebfaf36
Junosoft <junosoftltd@gmail.com>
2022-10-03 21:59:49 +0200

    file EDIT

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 
 const mongoose =  require("mongoose");
 mongoose.connect("mongodb://localhost/blog")
-.then (()=>console.log("Database is well connected")).catch(error =>console.log(error.message));
+.then (()=>console.log("DATABASE is well connected")).catch(error =>console.log(error.message));
 
 app.use(express.json());
 app.use('/',userRoute);
 
-app.listen(3000,()=>console.log("server is successfully connected"));
+app.listen(3000,()=>console.log("SERVER is successfully connected"));
